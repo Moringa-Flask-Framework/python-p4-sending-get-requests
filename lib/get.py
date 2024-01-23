799fb862c65254af02a8ec9d4e1c02142ecd06d5
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-01-23 19:32:13 +0300

    Changed url

--- lib/get.py
+++ lib/get.py
@@ -1,9 +1,10 @@
 import requests
 import json
 
-url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
+url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
 
 response = requests.get(url)
+# print(response.text)
 
 json_content = json.loads(response.text)
 
